80d4dfe19fd814f381ea4bd89581eae3d5808dc6
chen <chen@chendeMacBook-Pro.local>
2015-08-04 10:02:42 +0800

    Initial Commit

---
+++ mygit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mygit
+//
+//  Created by chen on 15/8/4.
+//  Copyright (c) 2015年 Benfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mygit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mygit
+//
+//  Created by chen on 15/8/4.
+//  Copyright (c) 2015年 Benfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
