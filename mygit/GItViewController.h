841eda7488f6a4a9abb68b404cf69fc39024802d
chen <chen@chendeMacBook-Pro.local>
2015-08-04 11:21:04 +0800

    create new vc

---
+++ mygit/GItViewController.h
@@ -0,0 +1,13 @@
+//
+//  GItViewController.h
+//  mygit
+//
+//  Created by chen on 15/8/4.
+//  Copyright (c) 2015年 Benfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GItViewController : UIViewController
+
+@end
